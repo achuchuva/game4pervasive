9f0e315007f52b52af9b1ac107a2289987d659a9
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2025-05-29 12:20:51 +1000

    shit

--- Game4Pervasive/Assets/Scripts/QuestManager.cs
+++ Game4Pervasive/Assets/Scripts/QuestManager.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using static QuestData;
+using UnityEngine.UI;
+using TMPro;
 
 public class QuestManager : MonoBehaviour
 {
@@ -9,60 +11,94 @@ public class QuestManager : MonoBehaviour
 
     public int selectedQuestIndex = 0;
 
+    // quest prefab
+    public GameObject questPrefab;
 
-    private void Awake()
+    public GameObject questBook;
+
+    public void Start()
     {
-        if (Instance == null) Instance = this;
+        // for testing we add some quests
+        QuestData quest1 = new QuestData
+        {
+            questId = 1,
+            questTitle = "Find the Lost Cat",
+            description = "Help the villagers find their lost cat.",
+            rewards = new Reward[] { }
+        };
+
+        activeQuests.Add(quest1);
+
+        // quest 2
+        QuestData quest2 = new QuestData
+        {
+            questId = 2,
+            questTitle = "Collect Herbs",
+            description = "Gather herbs from the forest for the healer.",
+            rewards = new Reward[] { }
+        };
+
+        activeQuests.Add(quest2);
+
+        // show
+        ShowQuestBook();
     }
 
-    public List<GameObject> GetAllQuestOptions()
+    public void ClearQuestPrefabs()
     {
-        // just get the children
-        var questOptions = new List<GameObject>();
+        // clear all quest prefabs
         foreach (Transform child in transform)
         {
-            questOptions.Add(child.gameObject);
+            Destroy(child.gameObject);
         }
-        return questOptions;
     }
 
-    public GameObject GetSelectedQuestOption()
+    // create quest prefabs
+    public void CreateQuestPrefabs()
     {
-        if (selectedQuestIndex < 0 || selectedQuestIndex >= transform.childCount)
-            return null;
-
-        return transform.GetChild(selectedQuestIndex).gameObject;
-    }
-
-    public QuestData GetSelectedQuest()
-    {
-        if (selectedQuestIndex < 0 || selectedQuestIndex >= activeQuests.Count)
-            return null;
-
-        return activeQuests[selectedQuestIndex];
-    }
-
-    public void StartQuest(QuestData quest)
-    {
-        if (!activeQuests.Contains(quest))
-            activeQuests.Add(quest);
-    }
-
-    public void CompleteQuest(QuestData quest, bool metExtraDetails)
-    {
-        activeQuests.Remove(quest);
-        if (!metExtraDetails) return;
-
-        var charData = quest.questGiver;
-        if (charData.approvalPoints < charData.maxApprovalPoints)
+        foreach (QuestData quest in activeQuests)
         {
-            charData.approvalPoints++;
-            UnlockContent(quest.rewards);
+            GameObject newQuest = Instantiate(questPrefab, transform);
+            Button questButton = newQuest.GetComponent<Button>();
+            TextMeshProUGUI buttonText = questButton.GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = quest.questTitle;
+            questButton.onClick.AddListener(() => OnOptionSelected(quest.questId));
         }
+    }
+
+    public void ShowQuestBook()
+    {
+        questBook.SetActive(true);
+        ClearQuestPrefabs();
+        CreateQuestPrefabs();
+
+        // get options container
+        Transform optionsContainer = gameObject.transform;
+
+        if (optionsContainer.childCount > 0)
+        {
+            var firstButton = optionsContainer.GetChild(0).GetComponent<Button>();
+            UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(firstButton.gameObject);
+        }
+    }
+
+    public void HideQuestBook()
+    {
+        questBook.SetActive(false);
     }
 
     private void UnlockContent(Reward[] rewards)
     {
         // TODO
+    }
+
+    public void OnOptionSelected(int optionId)
+    {
+        // set selected quest index
+        selectedQuestIndex = activeQuests.FindIndex(q => q.questId == optionId);
+
+
+        // handle option selection
+        Debug.Log($"Option {optionId} selected for quest {activeQuests[selectedQuestIndex].questTitle}");
     }
 }
