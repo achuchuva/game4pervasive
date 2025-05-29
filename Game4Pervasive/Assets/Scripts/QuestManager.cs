ab382d484b837da0744f4d2668d2dd884710fc9a
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2025-05-29 11:22:52 +1000

    Idk how to make it so that you can select options

--- Game4Pervasive/Assets/Scripts/QuestManager.cs
+++ Game4Pervasive/Assets/Scripts/QuestManager.cs
@@ -7,9 +7,39 @@ public class QuestManager : MonoBehaviour
     public static QuestManager Instance;
     public List<QuestData> activeQuests = new();
 
+    public int selectedQuestIndex = 0;
+
+
     private void Awake()
     {
         if (Instance == null) Instance = this;
+    }
+
+    public List<GameObject> GetAllQuestOptions()
+    {
+        // just get the children
+        var questOptions = new List<GameObject>();
+        foreach (Transform child in transform)
+        {
+            questOptions.Add(child.gameObject);
+        }
+        return questOptions;
+    }
+
+    public GameObject GetSelectedQuestOption()
+    {
+        if (selectedQuestIndex < 0 || selectedQuestIndex >= transform.childCount)
+            return null;
+
+        return transform.GetChild(selectedQuestIndex).gameObject;
+    }
+
+    public QuestData GetSelectedQuest()
+    {
+        if (selectedQuestIndex < 0 || selectedQuestIndex >= activeQuests.Count)
+            return null;
+
+        return activeQuests[selectedQuestIndex];
     }
 
     public void StartQuest(QuestData quest)
